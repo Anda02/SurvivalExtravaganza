c207972e4414dd7114a78b0c10371a8e13780435
Anda02 <anda02@gmail.com>
2018-10-26 12:50:33 -0400

    Setting up camera raycast for worldspace UI

---
+++ Assets/Scripts/CameraRaycast.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraRaycast : MonoBehaviour {
+
+    //Script is for generating a raycast from the camera in order to interact with various objects in the scene.
+	void Start () {
+		
+	}
+	
+    
+	public void OnPointerDown(PointerEventData evdata)
+    {
+        print(evdata.pointerPress);
+    }
+}
